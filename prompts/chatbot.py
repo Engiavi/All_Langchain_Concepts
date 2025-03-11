560648eb32d830422bcf0ea9b045105c9922ab56
Engiavi <hackavi796@gmail.com>
2025-03-12 00:06:00 +0530

    extended the  functionality of "messages" in terminal chatbot to get better response from the chat history

--- prompts/chatbot.py
+++ prompts/chatbot.py
@@ -1,15 +1,22 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
+
 load_dotenv()
+
 model = ChatGoogleGenerativeAI(model="gemini-2.0-flash-lite")
 
-chat_history = []
+chat_history = [
+    SystemMessage(content='You are a helpful AI assistant')
+]
+
 while True:
-    user_input = input("👦🏻: ")
-    chat_history.append(user_input)
-    if user_input == "exit":
+    user_input = input('👦🏻: ')
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input == 'exit':
         break
     result = model.invoke(chat_history)
-    chat_history.append(result.content)
-    print("🤖:",result.content) 
+    chat_history.append(AIMessage(content=result.content))
+    print("🤖: ",result.content)
+
 print(chat_history)
