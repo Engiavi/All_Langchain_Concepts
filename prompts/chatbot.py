a3e7aeb7cd00a19bf4f69dcb554e9b106471e4ed
Engiavi <hackavi796@gmail.com>
2025-03-11 23:38:00 +0530

    updating the code, so that it remembers the chat history

--- prompts/chatbot.py
+++ prompts/chatbot.py
@@ -3,9 +3,13 @@ from dotenv import load_dotenv
 load_dotenv()
 model = ChatGoogleGenerativeAI(model="gemini-2.0-flash-lite")
 
+chat_history = []
 while True:
     user_input = input("👦🏻: ")
+    chat_history.append(user_input)
     if user_input == "exit":
         break
-    result = model.invoke(user_input)
-    print("🤖:",result.content)
+    result = model.invoke(chat_history)
+    chat_history.append(result.content)
+    print("🤖:",result.content) 
+print(chat_history)
