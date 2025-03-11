9a5a9fd53940592a07b3b2c137fc3bd2f31877b1
Engiavi <hackavi796@gmail.com>
2025-03-11 21:47:17 +0530

    showing response from llm to the website

--- prompts/prompt_ui.py
+++ prompts/prompt_ui.py
@@ -2,11 +2,11 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 load_dotenv()
 import streamlit as st
-model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash-lite")
 
 st.header('Reasearch tool')
 user_input = st.text_input("Enter your prompt")
 
 if st.button("summarise"):
     result = model.invoke(user_input)
-    st.write("Some random text:")  
+    st.write(result.content)  
