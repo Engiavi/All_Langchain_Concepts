4c81dfd402e6a2f5b0dbe4cabded2f5355c43a7a
Engiavi <hackavi796@gmail.com>
2025-04-08 23:34:57 +0530

    code for Runnable branch, explicitly use for conditional branching of text and response of work flow

---
+++ Runnable/runnable_primitive/runnable_branch.py
@@ -0,0 +1,31 @@
+from langchain.schema.runnable import RunnableBranch,RunnableSequence,RunnablePassthrough #type:ignore
+from dotenv import load_dotenv #type:ignore
+from langchain_core.prompts import PromptTemplate #type:ignore
+from langchain_google_genai import ChatGoogleGenerativeAI #type:ignore
+from langchain_core.output_parsers import StrOutputParser #type:ignore
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+parser = StrOutputParser()
+
+prompt = PromptTemplate(
+    template = "write a detailed report on {topic}",
+    input_variables =['topic']
+)
+
+prompt2 = PromptTemplate(
+    template = "summarize the topic {topic}",
+    input_variables =['topic']
+)
+
+output1 = RunnableSequence(prompt,model,parser)
+
+branch_chain = RunnableBranch(
+    (lambda x: len(x.split())>500,RunnableSequence(prompt2,model,parser)),
+    RunnablePassthrough()
+)
+
+final_chain = RunnableSequence(output1,branch_chain)
+result = final_chain.invoke({"topic":"AI"})
+print(result)
