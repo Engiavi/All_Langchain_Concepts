84e0e44bb4b92ab985fa63d649faf8a5dac13f0d
Engiavi <hackavi796@gmail.com>
2025-04-12 15:46:01 +0530

    Added comments to improve readability

--- vector_stores/chroma_vector.py
+++ vector_stores/chroma_vector.py
@@ -23,16 +23,21 @@ vector_stores = Chroma.from_documents(
     persist_directory="my_chroma_db",
     collection_name="sample"
 )
-# vector_stores.add_documents(docs)
-# see_vector = vector_stores.get(include=['embeddings', 'documents', 'metadatas'])
+# add the documents to the vector store
+vector_stores.add_documents(docs)
+# getting the vector store(from chroma db)
+see_vector = vector_stores.get(include=['embeddings', 'documents', 'metadatas'])
 
-# print(see_vector)
-# print("Documents added to Chroma vector store.")
+# print the vector store
+print(see_vector)
+print("Documents added to Chroma vector store.")# basic print statement
 
-print(vector_stores.similarity_search(
+# getting the relevant query using similarity search
+print(vector_stores.similarity_search( 
     query="Who among is bowler",
     k=1,# returns the k most similar documents
 ))
+# getting the relevant query using similarity search with score(value should so small,means it lie nearby the query)
 print(vector_stores.similarity_search_with_score( 
     query="Who among is bowler",
     k=1,# returns the k most similar documents
@@ -47,21 +52,25 @@ print(vector_stores.similarity_search_with_score(
 #     filter={"source": "doc2"}
 # ))
 
-# update the document
+# creating the update document object
 updated_docs = Document(page_content="Ravindra jadeja is a all rounder and he is spinner", metadata={"source": "doc2"})
 
+# update the document in the vector store
 vector_stores.update_document(
     document_id = 'd5d2eb43-dab1-45ed-8479-c4cea3b652d2',
     document=updated_docs
 )
-print(vector_stores.similarity_search_with_score(
-    query="who is the spinner",
-    k=1, 
-))
-print("*********************")
-vector_stores.delete(ids = ['d5d2eb43-dab1-45ed-8479-c4cea3b652d2'])
+# again query the updated part
 print(vector_stores.similarity_search_with_score(
     query="who is the spinner",
     k=1, 
 ))
 
+print("*********************")
+# delete the document from the vector store
+vector_stores.delete(ids = ['d5d2eb43-dab1-45ed-8479-c4cea3b652d2'])
+# again query the updated part
+print(vector_stores.similarity_search_with_score(
+    query="who is the spinner",
+    k=1, 
+))
--- vector_stores/yt.py
+++
@@ -1,8 +0,0 @@
-import onnxruntime as ort
-
-try:
-    print("ONNX Runtime version:", ort.__version__)
-    sess = ort.InferenceSession  # just check it's accessible
-    print("ONNXRuntime loaded successfully.")
-except Exception as e:
-    print("ONNXRuntime failed to load:", e)
