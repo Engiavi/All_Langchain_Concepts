ba001da720d47978bf726bc466c53addb38827f6
Engiavi <hackavi796@gmail.com>
2025-05-03 12:22:05 +0530

    adding a functionality of tool using functions and tool decorator

--- ai-agents/search-agent.py
+++ ai-agents/search-agent.py
@@ -9,3 +9,16 @@ load_dotenv()
 
 # built-in tool- it is a search tool which is used to search the web for information. It uses the DuckDuckGo search engine to perform the search and returns the results in a structured format.
 search_tool = DuckDuckGoSearchRun() 
+
+# tool decorator
+@tool
+# it basically give response of the weather data from the weatherstack API
+def get_weather_data(city: str) -> str:
+  """
+  This function fetches the current weather data for a given city
+  """
+  url = f'https://api.weatherstack.com/current?access_key=4d1d8ae207a8c845a52df8a67bf3623e&query={city}'
+
+  response = requests.get(url)
+
+  return response.json()
