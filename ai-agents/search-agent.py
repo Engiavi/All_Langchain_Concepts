2b717c39b96c2320a6da9c2b5ec9d8c4fe9c5cad
Engiavi <hackavi796@gmail.com>
2025-05-03 12:17:14 +0530

    built-in tool -- duckducksearch engine

--- ai-agents/search-agent.py
+++ ai-agents/search-agent.py
@@ -6,3 +6,6 @@ from langchain.agents import create_react_agent, AgentExecutor
 from langchain import hub
 from dotenv import load_dotenv
 load_dotenv()
+
+# built-in tool- it is a search tool which is used to search the web for information. It uses the DuckDuckGo search engine to perform the search and returns the results in a structured format.
+search_tool = DuckDuckGoSearchRun() 
