f4fa58bd491dbe5118ababb6cb4e94b66fe0e9de
Engiavi <hackavi796@gmail.com>
2025-05-03 12:06:03 +0530

    importing necessary library

---
+++ ai-agents/search-agent.py
@@ -0,0 +1,8 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.tools import tool
+import requests
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain import hub
+from dotenv import load_dotenv
+load_dotenv()
